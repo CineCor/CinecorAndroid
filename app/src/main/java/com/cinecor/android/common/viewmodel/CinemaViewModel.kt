6d8b0be238e8ef29ed27c3a144d58f1fbf7f36b2
Gonzalo Toledano <gonzalotldn@gmail.com>
2017-08-17 21:34:15 +0100

    Now using LiveDataReactiveStreams to convert Flowable to LiveData

--- app/src/main/java/com/cinecor/android/common/viewmodel/CinemaViewModel.kt
+++ app/src/main/java/com/cinecor/android/common/viewmodel/CinemaViewModel.kt
@@ -1,7 +1,7 @@
 package com.cinecor.android.common.viewmodel
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.LiveDataReactiveStreams
 import android.arch.lifecycle.ViewModel
 import com.cinecor.android.data.model.Cinema
 import com.cinecor.android.data.model.Movie
@@ -9,21 +9,12 @@ import com.cinecor.android.data.source.CinemasRepository
 
 class CinemaViewModel(private val repository: CinemasRepository) : ViewModel() {
 
-    fun getCinemas(): LiveData<List<Cinema>> {
-        val cinemas = MutableLiveData<List<Cinema>>()
-        repository.getCinemas().doOnNext { cinemas.value = it }
-        return cinemas
-    }
+    fun getCinemas(): LiveData<List<Cinema>> =
+            LiveDataReactiveStreams.fromPublisher(repository.getCinemas())
 
-    fun getCinema(cinemaId: Int): LiveData<Cinema> {
-        val cinema = MutableLiveData<Cinema>()
-        repository.getCinema(cinemaId).doOnNext { cinema.value = it }
-        return cinema
-    }
+    fun getCinema(cinemaId: Int): LiveData<Cinema> =
+            LiveDataReactiveStreams.fromPublisher(repository.getCinema(cinemaId))
 
-    fun getMovieFromCinema(movieId: Int, cinemaId: Int): LiveData<Movie> {
-        val movie = MutableLiveData<Movie>()
-        repository.getMovieFromCinema(movieId, cinemaId).doOnNext { movie.value = it }
-        return movie
-    }
+    fun getMovieFromCinema(movieId: Int, cinemaId: Int): LiveData<Movie> =
+            LiveDataReactiveStreams.fromPublisher(repository.getMovieFromCinema(movieId, cinemaId))
 }
